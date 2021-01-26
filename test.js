80dd38595872b942044c989ce0c3f9a9e7eaa004
unknown <miecie-1908@hotmail.com>
2021-01-26 13:23:43 +0200

    first commit

---
+++ test.js
@@ -0,0 +1,9 @@
+
+function initiate(){
+    //console.log("assdf");
+    var headerImage = document.getElementById("header-eng");
+    console.log(headerImage);
+    headerImage.addEventListener("click", function(){
+        alert("Θέλετε να αλλάξετε γλώσσα;")
+    }); 
+}
